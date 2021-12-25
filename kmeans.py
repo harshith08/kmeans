cadd4b815ca3ffbd5a03dc3db022e7485cdb0bad
harshith08 <harshithpasumarthi1@gmail.com>
2021-12-24 22:03:25 -0500

    alters

--- kmeans.py
+++ kmeans.py
@@ -1,18 +1,24 @@
 import pandas as pd
 import numpy as np
+import random as rd
 from distance import euclidieanDistance
 class kMeans:
-    def __init__(self,name,age):
-        self.x = name
-        self.y = age
-    def calculation(self, value):
-        self.tot = value
-        notvalue=8
-        print(self.tot,",",notvalue)
+    def __init__(self):
+       pass
+    def randomIntegers(self,k,rows):
+        rand_array = rd.sample(rows,k=k)
+        return rand_array
     def another(self):
         print(self.tot)
     if __name__ == "__main__":
+        k=2
         df = pd.read_csv("Mall_Customers.csv")
-        
-ob1 = kMeans("Harshith",23)
+        col = []
+        for i in range(len(df.columns)):
+            col.append(str(df.columns[i]))
+        data = np.array(df[col])
+        rows = [i for i in range(len(data))]
+        result = randomIntegers(k,rows)
+        print(result)
+ob1 = kMeans()
 
